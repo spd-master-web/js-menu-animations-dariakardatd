d296efc6d088ad7d656ec4d013abe778089d7b28
dariakardatd <kardatd2@mail.scuoladesign.com>
2021-07-12 12:27:08 +0200

    animation

--- js/one.js
+++ js/one.js
@@ -4,9 +4,55 @@ document.addEventListener('DOMContentLoaded', () => {
 
   hamburger.addEventListener('click', (e) => {
     // animate here when open the menu
+    // gsap.to(".hamburger ", {
+ 
+
+
+    //   rotation: 720,
+    //   x: 100,
+    //   duration: 3,
+    //   repeat: 3,
+    // }, {
+    //   rotation: 0,
+    //   x: -50,
+    //   duration: 3,
+    //   repeat: 3,
+    // });
+
+
+    gsap.to("aside", {
+      x: 0,
+    });
+
+    gsap.to("li", {
+
+      opacity: 100,
+      delay: 1,
+      stagger: 0.1,
+    });
+
+gsap.to(".copy", {
+  opacity: 100,
+  x: 0,
+  duration: 2,
+  ease: 'back'
+});
+
+gsap.to(".close", {
+  y:0,
+  
+});
+
   })
 
   close.addEventListener('click', () => {
     // animate here when close the menu
+    gsap.to("aside", {
+      x: -600,
+    });
+
+    gsap.to(".close", {
+      y:-200,
+    })
   })
 })
